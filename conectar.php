50ba05fffb0a579094b477e006fc4806de29f078
Santiago Urdaneta <santurdaneta@gmail.com>
2017-09-16 11:36:27 -0400

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -2,13 +2,13 @@
    define('DB_SERVER', '127.0.0.1');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '123456');
-   define('DB_DATABASE', 'constructora');
+   define('DB_DATABASE', 'asistencia');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
    $servername = "127.0.0.1";
 $username = "root";
 $password = "123456";
-$dbname = "constructora";
+$dbname = "asistencia";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -20,7 +20,7 @@ if ($conn->connect_error) {
 $hostname = "127.0.0.1";
 $username = "root";
 $password = "123456";
-$databaseName = "constructora";
+$databaseName = "asistencia";
 
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 ?>
